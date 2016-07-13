64201d22d114b6ba535e31e9a602ed2e5ff1d887
Daniel Huddleston <djohnhuddleston@gmail.com>
2016-07-13 19:26:44 +0000

    fix 'cannot get /' error for url creation

--- controllers/api.js
+++ controllers/api.js
@@ -24,27 +24,32 @@ module.exports = function(app, mongoose){
        }); 
     });
     
-    app.get('/new/:url', postUrl);
+    app.get('/new/:url*', postUrl);
     
     function postUrl(req, res){
         var shortUrl = new ShortUrl();
-        var url = req.url;
+        var url = req.url.slice(5);
         if(validUrl.isUri(url))
         {
             shortUrl.originalUrl=url;
             shortUrl.shortenedUrl= process.env.APP_URL + makeShortUrl();
             shortUrl.error = null;
             
-            shortUrl.save(function(err){
+            shortUrl.save(function(err, data){
                if(err)
                {
                    res.send(err);
+               }
+               else
+               {
+                   res.json(data);
                }
             });
         }
         else
         {
             res.send("Not a valid URL. Please try again");
+            console.log(url);
         }
     }
     
